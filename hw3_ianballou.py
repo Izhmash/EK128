04d3eec94125501183334ab453d3f9063960628b
Izhmash <ianballou67@gmail.com>
2016-03-02 17:18:56 -0500

    work on hw3

--- hw3_ianballou.py
+++ hw3_ianballou.py
@@ -9,10 +9,25 @@ def word_count(mytext):
         else:
             words[i] = 1
     return words
-# print(word_count("This(is)a test.this.is.a.test THiS is-a-Test TeSting"))
+print(word_count("This is a a a a a a test"))
 
 
 def analyze(count_dictionary):
-    return
+    sortedByKey = sorted(count_dictionary, reverse=True)
+    sortedByValues = sorted(count_dictionary.values(), reverse=True)
+    mostFreq = []
+    longest = []
+    longerThan1Count = 0
+    for i in range(0, len(sortedByKey)):
+        if len(sortedByKey[i]) == len(sortedByKey[0]):
+            longest.append(sortedByKey[i])
+        if len(sortedByKey[i]) > 1:
+            longerThan1Count += 1
+        
+        
+    # print(sortedDictKey)
+    # print(sortedDictValues)
+analyze(word_count("This is a a a a a a(test!"))
+
 
 # def top_words(count_dictionary):
